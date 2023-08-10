95c384134c0d0b6d5d24d15906f789825875d6fd
Ab IHSSANE <96747419+Ihssaneabousshal@users.noreply.github.com>
2023-08-10 21:19:59 +0100

    Create exec.tf

---
+++ exec.tf
@@ -0,0 +1,17 @@
+data "template_file" "inventory" {
+  template = <<-EOT
+    [ec2_instances]
+    ${aws_instance.public_instance.public_ip} ansible_user=ubuntu ansible_private_key_file=${path.module}/makey
+    EOT
+}
+
+resource "local_file" "dynamic_inventory" {
+  depends_on = [aws_instance.public_instance]
+
+  filename = "dynamic_inventory.ini"
+  content  = data.template_file.inventory.rendered
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${local_file.dynamic_inventory.filename}"
+  }
+}
