7cdd49155b71a7aea0e72b82d1cff1f0a5267fdd
Ab IHSSANE <96747419+Ihssaneabousshal@users.noreply.github.com>
2023-08-10 20:39:51 +0100

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -3,22 +3,19 @@ resource "tls_private_key" "rsa_4096" {
   rsa_bits  = 4096
 }
 
-variable "key_name" {
-  description = "Name of the SSH key pair"
-}
 
 // Create Key Pair for Connecting EC2 via SSH
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
+  key_name   = "Akey"
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
 // Save PEM file locally
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
-  filename = var.key_name
+  filename = "Akey"
 
   provisioner "local-exec" {
-    command = "chmod 400 ${var.key_name}"
+    command = "chmod 400 Akey"
   }
 }
