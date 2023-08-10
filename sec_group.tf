a35be7ae0c9e4ec7eeb5886d7f4f577c5116c80e
Ab IHSSANE <96747419+Ihssaneabousshal@users.noreply.github.com>
2023-08-10 20:26:06 +0100

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -1,1 +1,25 @@
+resource "aws_security_group" "ec2sg" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
