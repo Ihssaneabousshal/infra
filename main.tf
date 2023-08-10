beb8c829a49426d63e5d4471bf59fd0e04367470
Ab IHSSANE <96747419+Ihssaneabousshal@users.noreply.github.com>
2023-08-10 20:36:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
+ key_name = aws_key_pair.key_pair.key_name
  vpc_security_group_ids = [aws_security_group.ec2sg.id]
 
  tags = {
