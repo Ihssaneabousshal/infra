c5994738a7881c6967d67db88f15b434d7533c89
Ab IHSSANE <96747419+Ihssaneabousshal@users.noreply.github.com>
2023-08-10 23:33:18 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,9 @@
 resource "aws_s3_bucket" "inventory_bucket" {
-  bucket = "Abucketforinventory"
-  acl    = "private"  # Adjust ACL as needed
+  bucket = "apiappinventorybucket"
 }
+
 resource "aws_s3_bucket_object" "inventory_object" {
   bucket = aws_s3_bucket.inventory_bucket.id
   key    = "dynamic_inventory.ini"
   content = data.template_file.inventory.rendered
-  acl    = "private"  # Adjust ACL as needed
 }
