2987944330e38712e3641de62f17a6ed9f88c8d2
Ab IHSSANE <96747419+Ihssaneabousshal@users.noreply.github.com>
2023-08-12 21:18:22 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
-data "aws_s3_bucket" "inventory_bucket" {
+resource "aws_s3_bucket" "inventory_bucket" {
   bucket = "apiappinventorybucket"
 }
 
 resource "aws_s3_bucket_object" "inventory_object" {
-  bucket = data.aws_s3_bucket.inventory_bucket.id
+  bucket = aws_s3_bucket.inventory_bucket.id
   key    = "dynamic_inventory.ini"
   content = data.template_file.inventory.rendered
-}
+} 
